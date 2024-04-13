36b2c7a514112537bc461f551cdd895bdd6ba9b0
Aadido <147530344+Aadido@users.noreply.github.com>
2024-04-13 09:38:36 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name= "Ranbir";
+console.log(first_name)
+var last_name= "Kapoor";
+console.log(last_name)
+var full_name= first_name.concat(last_name);
+console.log(full_name)
+var a= 10
+console.log(a)
+var b= 5
+console.log(b)
+var sum= a + b
+console.log(sum)
+var product= a*b
+console.log(product)
+var quotient= a/b
+console.log(quotient)
